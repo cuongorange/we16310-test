53534573b3df49638daef3dc847f087d15a4ebdf
cuongcam <cuon9cam@gmail.com>
2022-01-21 10:56:50 +0700

    init project

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1 class="bg-red-500"> Hello cuong cam !</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
